2b9f1c4f4f2fa6970dc1492ef4e4644a5cb1df85
Lotus <D24091111111@cityu.edu.mo>
2024-09-16 03:51:42 +0800

    Update ClassEx1.cpp

--- ClassEx/ClassEx1.cpp
+++ ClassEx/ClassEx1.cpp
@@ -9,10 +9,10 @@ int main(){
   delta=b*b-4*a*c;
   if(a==0)
       cout<<"不是二次方程"<<endl;
-  else if(d==0)
+  else if(delta==0)
       cout<<"有两个相等的实根"<<endl;
 
-  else if(d>0)
+  else if(delta>0)
       cout<<"有两个不等的实根"<<endl;
 
   else
